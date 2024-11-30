736351670a1e681c91e5cd3a1a28e47b6114c5d9
Edward Banner <edward.banner@gmail.com>
2024-11-30 17:37:57 -0500

    ✅ OS Dev: Print a character to the screen

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+run: build
+	qemu-system-i386 -nographic boot_sect.bin
+
 build:
 	nasm boot_sect.asm -f bin -o boot_sect.bin
 
--- boot_sect.asm
+++ boot_sect.asm
@@ -1,21 +1,29 @@
 ;
-; A simple boot sector program that loops forever.
+; A simple boot sector that prints a message to the screen using a BIOS routine.
 ;
 
-loop:                       ; Define a label, "loop", that will allow ; us to
-                            ; jump back to it, forever.                                       
+main:
+  mov ah, 0x0e              ; int 10/ah = 0eh -> scrolling teletype BIOS routine
+  mov al, 'E'
+  int 0x10
 
-    jmp loop                ; Use a simple CPU instruction that jumps                                                                         
-                            ; to a new memory address to continue execution.
-                            ; In our case, jump to the address of the current
-                            ; instruction. 
-    
+  jmp done
 
-    times 510-($-$$) db 0   ; When compiled, our program must fit into 512 bytes,                    
-                            ; with the last two bytes being the magic number,                        
-                            ; so here, tell our assembly compiler to pad out our                     
-                            ; program with enough zero bytes (db 0) to bring us
-                            ; to the 510th byte. 
+done:
+  mov ah, 0x0e              ; int 10/ah = 0eh -> scrolling teletype BIOS routine
+  mov al, '.'
+  int 0x10
+  jmp $
 
-    dw 0xaa55               ; Last two bytes (one word) form the magic number,
-                            ; so BIOS knows we are a boot sector.
+;
+; Padding and magic BIOS number.
+;
+
+times 510-($-$$) db 0   ; When compiled, our program must fit into 512 bytes,                    
+                        ; with the last two bytes being the magic number,                        
+                        ; so here, tell our assembly compiler to pad out our                     
+                        ; program with enough zero bytes (db 0) to bring us
+                        ; to the 510th byte. 
+
+dw 0xaa55               ; Last two bytes (one word) form the magic number,
+                        ; so BIOS knows we are a boot sector.
